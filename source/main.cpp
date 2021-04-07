9a225a361cac6ad68c2d7e07a9c89b85638402cd
Ben McGraw <ben.mcgraw@gmail.com>
2021-04-06 17:33:35 -0700

    Re-add productdir as an include base

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 #include "SDL.h"
-//#include "third_party/JSON/json.hpp"
+#include "third_party/JSON/json.hpp"
 
 #include "sdl_setup.h"
 
 using namespace std;
-//using json = nlohmann::json;
+using json = nlohmann::json;
 
 int SDL_main(int argc, char **argv) {
-	//auto j3 = json::parse(R"({"happy": true, "pi": 3.141})");
+	auto j3 = json::parse(R"({"happy": true, "pi": 3.141})");
 
-	//cout << j3["pi"] << SDL_atoi("a") << endl;
+	cout << j3["pi"] << SDL_atoi("a") << endl;
 
 	init();
 	loop();
